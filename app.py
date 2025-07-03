05f8e44af7cc3a9dbc5ff6074d9e3d57e8faac8d
jafferlisboa <jafferlisboa@outlook.com>
2025-07-03 12:02:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, send_file, abort, render_template
+import os
+import qrcode
+import io
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    return render_template("index.html")
+
+@app.route('/qr/<session_id>')
+def mostrar_qr(session_id):
+    path = f"./qrcodes/{session_id}.txt"
+    if not os.path.exists(path):
+        return abort(404, "QR Code ainda não foi gerado.")
+
+    with open(path, "r") as f:
+        qr_data = f.read()
+
+    img = qrcode.make(qr_data)
+    buf = io.BytesIO()
+    img.save(buf)
+    buf.seek(0)
+    return send_file(buf, mimetype="image/png")
