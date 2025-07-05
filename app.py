c38bc6beeabd18c4ec311e010b560c322c3bc524
jafferlisboa <jafferlisboa@outlook.com>
2025-07-05 11:52:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_PORT = os.getenv("DB_PORT")
-
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 app = Flask(__name__)
